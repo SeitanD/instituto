43893df336ec9961d8b2d36149c11de7bf8fa385
Sebastian Diaz <sn.diaz@duocuc.cl>
2024-06-22 11:53:50 -0400

    CRUD

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index,crud
-from . import views
+from .views import index, crud, alumnosAdd, alumnos_del, alumnos_findEdit, alumnosUpdate
 
 urlpatterns = [
-    path('',index,name='index'),
-    path('crud',views.crud, name='crud'),
+    path('', index, name='index'),
+    path('crud', crud, name='crud'),
+    path('alumnosAdd', alumnosAdd, name='alumnosAdd'),
+    path('alumnos_del/<str:pk>', alumnos_del, name='alumnos_del'),
+    path('alumnos_findEdit/<str:pk>', alumnos_findEdit, name='alumnos_findEdit'),
+    path('alumnosUpdate', alumnosUpdate, name='alumnosUpdate'),
+
 
 ]
